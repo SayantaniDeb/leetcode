2dddc2b146ed739fea6e8f6f89506431e025769f
Sayantani Deb <sayantani.1001@gmail.com>
2025-12-12 09:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (46.63%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        res=[]
+        for c in s:
+            if c.isdigit():
+                res.pop()
+            else:
+                res.append(c)
+        return "".join(res)
+                
+
+        
